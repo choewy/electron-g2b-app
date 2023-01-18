cdd2ece1172fb76675a2a30b15c838ea866d19b1
choewy <choewy32@gmail.com>
2023-01-18 13:03:41 +0900

    index.ts

---
+++ src/index.tsx
@@ -0,0 +1,31 @@
+import './index.css';
+
+import { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { App } from '@/app';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <RecoilRoot>
+    <Suspense fallback={<div>LOADING</div>}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </Suspense>
+  </RecoilRoot>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
