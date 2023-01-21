3a665ddf64c316c0868053ac007ef5b61bb7fb95
choewy <choewy32@gmail.com>
2023-01-21 17:01:33 +0900

    craco setup

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,34 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
   webpack: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+    alias: { '@': path.resolve(__dirname, 'src') },
+    configure: (config) => {
+      const fallback = config.resolve.fallback || {};
+
+      Object.assign(fallback, {
+        path: require.resolve('path-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        assert: require.resolve('assert'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify'),
+        url: require.resolve('url'),
+        child_process: false,
+        fs: false,
+      });
+
+      config.resolve.fallback = fallback;
+      config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+      ]);
+
+      return config;
     },
   },
 };
