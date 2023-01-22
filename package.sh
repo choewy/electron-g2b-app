2872955be01a2324c27a8d261d0b7f9d0ef4bf62
choewy <choewy32@gmail.com>
2023-01-22 17:06:33 +0900

    migrate to react-app

---
+++ package.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+npm i craco @craco/craco recoil universal-cookie styled-components @emotion/react @emotion/styled @mui/icons-material @mui/material @mui/styled-engine-sc @mui/x-date-pickers axios luxon react-helmet-async react-router-dom google-spreadsheet
+npm i -D @types/google-spreadsheet @types/luxon @types/react-window assert buffer net process stream-http tls url web3-utils crypto-browserify https-browserify os-browserify path-browserify stream-browserify
