e92063fe8dbe1dd0141ce39c776cfb40c872d2a5
choewy <choewy32@gmail.com>
2023-01-25 13:22:04 +0900

    feature/keyword-counts-at-label

--- src/page/search/keyword/keyword.tabs.tsx
+++ src/page/search/keyword/keyword.tabs.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react';
 import { Tab, Tabs } from '@mui/material';
 import { KeywordTabsProps } from './types';
+import { keywordStore } from '@/store';
 
 export const KeywordTabs: FC<KeywordTabsProps> = ({
   keywordType,
   onChange,
 }) => {
+  const keywords = keywordStore.useValue();
+  const includes = (keywords.include || []).length;
+  const excludes = (keywords.exclude || []).length;
+
   return (
     <Tabs value={keywordType} onChange={onChange} centered>
-      <Tab value="include" label="검색 키워드" />
-      <Tab value="exclude" label="제외 키워드" />
+      <Tab value="include" label={`검색 키워드(${includes})`} />
+      <Tab value="exclude" label={`제외 키워드(${excludes})`} />
     </Tabs>
   );
 };
