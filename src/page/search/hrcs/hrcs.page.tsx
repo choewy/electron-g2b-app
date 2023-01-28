f775f451974d6d88ad516e6a6ad45698cff74027
choewy <choewy32@gmail.com>
2023-01-28 15:31:26 +0900

    refactor/hrcs-page-hooks

--- src/page/search/hrcs/hrcs.page.tsx
+++ src/page/search/hrcs/hrcs.page.tsx
@@ -1,16 +1,9 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { Box } from '@mui/material';
 import { KeywordBox } from '../keyword';
 import { HrcsSearchForm } from './hrcs.form';
-import { hrcsSearchStore } from '@/store';
 
 export const HrcsSearchPage: FC = () => {
-  const resetState = hrcsSearchStore.useResetState();
-
-  useEffect(() => {
-    resetState();
-  }, [resetState]);
-
   return (
     <Box component="div" display="flex" sx={{ padding: 1, height: 600 }}>
       <HrcsSearchForm />
