721e9e5d3c905c990b7130b6237bc4c7bfd4a310
choewy <choewy32@gmail.com>
2024-03-02 07:16:18 +0900

    refactor/gh-pages.js

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ fs.writeFileSync(
   './package.json',
   JSON.stringify(
     Object.assign(JSON.parse(fs.readFileSync('./package.json').toString('utf-8')), {
-      homepage: 'https://choewy.github.io/g2b',
+      homepage: 'https://choewy.github.io/g2b-client',
     }),
     null,
     2,
