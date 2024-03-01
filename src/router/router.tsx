fd7decafa349ded2556f342c9c5636d9ffdc02b4
choewy <choewy32@gmail.com>
2024-03-02 07:05:17 +0900

    feature/gh-pages

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -14,53 +14,56 @@ import { HomePage } from '@page/home/home.page';
 import { EmailVerificationPage } from '@page/email-verification/email-verification.page';
 import { ResetPasswordPage } from '@page/reset-password/reset-password.page';
 
-export const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        element: <AuthGuard requiredAuth={false} />,
-        children: [
-          {
-            path: RouterPath.SignIn,
-            element: <SignInPage />,
-          },
-          {
-            path: RouterPath.SignUp,
-            element: <SignUpPage />,
-          },
-          {
-            path: RouterPath.ResetPassword,
-            element: <ResetPasswordPage />,
-          },
-        ],
-      },
-      {
-        element: (
-          <>
-            <AuthGuard requiredAuth={true} />
-            <EmailVerifyGuard />
-          </>
-        ),
-        children: [
-          {
-            path: RouterPath.Home,
-            element: <HomePage />,
-          },
-          {
-            path: RouterPath.EmailVerification,
-            element: <EmailVerificationPage />,
-          },
-          {
-            path: RouterPath.SignOut,
-            element: <SignOutPage />,
-          },
-        ],
-      },
-      {
-        path: '*',
-        element: <Navigate to={RouterPath.Home} replace />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      element: <App />,
+      children: [
+        {
+          element: <AuthGuard requiredAuth={false} />,
+          children: [
+            {
+              path: RouterPath.SignIn,
+              element: <SignInPage />,
+            },
+            {
+              path: RouterPath.SignUp,
+              element: <SignUpPage />,
+            },
+            {
+              path: RouterPath.ResetPassword,
+              element: <ResetPasswordPage />,
+            },
+          ],
+        },
+        {
+          element: (
+            <>
+              <AuthGuard requiredAuth={true} />
+              <EmailVerifyGuard />
+            </>
+          ),
+          children: [
+            {
+              path: RouterPath.Home,
+              element: <HomePage />,
+            },
+            {
+              path: RouterPath.EmailVerification,
+              element: <EmailVerificationPage />,
+            },
+            {
+              path: RouterPath.SignOut,
+              element: <SignOutPage />,
+            },
+          ],
+        },
+        {
+          path: '*',
+          element: <Navigate to={RouterPath.Home} replace />,
+        },
+      ],
+    },
+  ],
+  { basename: process.env.PUBLIC_URL },
+);
